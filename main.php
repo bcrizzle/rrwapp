9fdb2c3449d13c072d69e55baa34c3fcfbe7ca90
bcrizzle <brandon.crane@lethbridgecollege.ca>
2021-02-03 03:20:51 -0700

    began setting up UI framework.

---
+++ main.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+    <title>Rustik Reclamations MGMNT</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</head>
+<body style="background-color: whitesmoke;">
+<div style="width: 100%; text-align: center;">
+    <img class="img-fluid " src="res/images/RRLogo.jpg" style="margin-left: auto;margin-right: auto; max-width: 10%;">
+</div>
+<div style="margin-left: auto; margin-right: auto; margin-top: 10%; display: flex">
+    <div class="card" style="width: 18rem; margin-right: 5%; margin-left: auto">
+        <img src="res/images/order-image.png" class="card-img-top" alt="Client Image">
+        <div class="card-body">
+            <h5 class="card-title">Orders</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="orders.php" class="btn btn-primary">Manage Orders</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;margin-right: 5%; margin-left: 5%">
+        <img src="res/images/client-image.png" class="card-img-top" alt="Client Image">
+        <div class="card-body">
+            <h5 class="card-title">Client</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="client.php" class="btn btn-primary">Manage Clientele</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;margin-right: auto; margin-left: 5%">
+        <img src="res/images/inventory-image.png" class="card-img-top" alt="Client Image">
+        <div class="card-body">
+            <h5 class="card-title">Inventory</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="inventory.php" class="btn btn-primary">Manage Inventory</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ orders.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <title>Rustik Reclamations MGMNT</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</head>
+<body style="background-color: whitesmoke;">
+<div style="width: 100%; text-align: center;">
+    <img class="img-fluid " src="res/images/RRLogo.jpg" style="margin-left: auto;margin-right: auto; max-width: 10%;">
+</div>
+<h1>Orders</h1>
+</body>
+</html>
